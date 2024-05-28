9d666fffbfc0c39e15ce7e990ca582ccd50bee77
Nathan <143134702+nathanseixas@users.noreply.github.com>
2024-05-28 18:11:09 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const connection = createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: 3306
+    port: 53231
 });
 
 connection.connect((error)=> {
