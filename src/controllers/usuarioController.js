ea8a4a281806389e61a36a8d50d79d3fc79447c9
Danilosrr <danilo_srr@hotmail.com>
2024-05-28 23:42:31 -0300

    login

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -17,6 +17,18 @@ import usuarioServices from "../services/usuarioServices.js";
 
     }
 
+    export async function logarUsuario(req, res) {
+        let json = {error: '', result:{}}
+        const {nome, cpf} = req.body;
+        let usuario = await usuarioServices.buscarUm(nome);
+        if(usuario.cpf == cpf){
+            json.result = usuario
+        }else {
+            json.error = 'Usuário nao encontrado'
+        }
+        return res.json(json);
+    }
+
 
    
     
--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,14 @@ const router = Router();
 
 import { buscarTodas, buscarUma, inserir, receita_usuario, deletarReceitas } from './controllers/ReceitaController.js';
 import validarUsuario from './middlewares/token.js';
-import { criarUsuarios } from './controllers/usuarioController.js';
+import { criarUsuarios, logarUsuario } from './controllers/usuarioController.js';
 
 router.get('/receita', buscarTodas);
 router.get('/receita/:codigo', buscarUma);
 router.post('/receita', inserir);
 router.get('/receita/usuario/:codigo', receita_usuario);
 router.post('/usuario', criarUsuarios);
+router.post('/login', logarUsuario);
 router.delete('/receita/:codigo', validarUsuario, deletarReceitas);
 
 export default router;
